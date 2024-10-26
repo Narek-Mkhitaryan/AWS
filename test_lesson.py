3abc2b2bf162e7da8eaf8b5462627eb3bb828ddb
Narek <I19.mkhitaryan.narek@etud.ufar.am>
2024-10-27 02:46:48 +0400

    Task accomplished

--- test_lesson.py
+++ test_lesson.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+
 def main():
-    return 'hello world'
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8080)
