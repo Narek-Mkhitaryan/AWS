174ded5b4d91be835df648639242b5b6dabd3f1c
Narek <I19.mkhitaryan.narek@etud.ufar.am>
2024-12-11 02:42:19 +0400

    Final work

--- main.py
+++ main.py
@@ -3,15 +3,7 @@ app = Flask("__name__")
 
 @app.route('/')
 def index():
-    return render_template("index.html", active_page='projects')
-@app.route('/skills')
-def skills():
-    return render_template("skills.html", active_page='skills')
-
-
-@app.route('/contacts')
-def contacts():
-    return render_template("contacts.html", active_page='contacts')
+        return render_template("index.html")
 
 @app.route('/download')
 def download():
@@ -19,4 +11,4 @@ def download():
     return send_file(path, as_attachment=True)
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8080)
